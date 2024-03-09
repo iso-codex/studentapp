6720d1e9ed6330c35e750f557be48c65a95ed2d1
iso-codex <108698605+iso-codex@users.noreply.github.com>
2024-03-08 17:17:54 -0800

    Update test\CourseService.java

--- test/CourseServiceTest.java
+++ test/CourseServiceTest.java
@@ -12,16 +12,38 @@ public class CourseServiceTest {
 			 
 			CourseService service = new CourseService() ;
 			testReturnAllCourses(service);
+			testFindByID(service);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	 }
 	 private static void testReturnAllCourses(CourseService svc) throws Exception {
+		 System.out.println("Testing Return all courses\n");
 		 Course[] courses = svc.returnAllCourses();
 		 for(Course c: courses) {
 			 System.out.println(c.getId() + " " + c.getCourseName());
 			 
+		 }
+		 
+		 
+		 
+	 }
+	 
+	 private static boolean testFindByID(CourseService svc) throws Exception {
+		 String courseID = "Science";
+		 Course foundCourse = null;
+		 System.out.println("\n\nTesting CourseService.findByID");
+		 
+		 foundCourse = svc.findCourseById(courseID);
+		
+		 if(foundCourse == null) {
+			 System.out.println("Test failed" +  " " + courseID + " Course ID not found");
+			 return false;
+		 }
+		 else {
+			 System.out.println("Test passed" + " "+ courseID + " Course ID found");
+			 return true;
 		 }
 		 
 		 
